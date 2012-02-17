1e1cc398e3094a4d52844feac4750f2bb7970507
Tom Brow <tebrow@gmail.com>
2012-02-17 04:50:34 -0600

    parse multiple trees

--- parse_test.hs
+++ parse_test.hs
@@ -7,11 +7,17 @@ import Text.Show.Pretty (ppShow)
 
 data Tree = Node String [Tree] deriving Show
 
-main = parseFromFile tree "file.txt" >>= putStrLn . ppShow
+main = parseFromFile file "file.txt" >>= putStrLn . ppShow
+
+file :: PydentParser st [Tree]
+file = do
+  xs <- many (try tree)
+  return xs
 
 tree :: PydentParser st Tree
 tree = do
-  str <- many (noneOf "\n")
+  skipMany (try emptyLine)
+  str <- many1 (noneOf "\n")
   newline
   children <- optionMaybe (try (manyIndented tree))
   return (Node str (fromMaybe [] children))
